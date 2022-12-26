2a50ca804273d2f983f7e2d00d8365101371296e
Яков <yvipanov@sberbank.ru>
2022-12-26 05:48:45 +0300

    добавлено быстрое создание сервера

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-export * from './server';
+import { FoxServer, FoxServerProps } from './server';
+
+export function createFoxServer(options?: FoxServerProps) {
+    return new FoxServer(options);
+}
+
+export { FoxServer, FoxServerProps };
